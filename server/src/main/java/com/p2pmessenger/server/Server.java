a00d1388e3e7d835833ae7340a567440eac06a10
aarondominguezcostas <aarondominguezcostas@gmail.com>
2021-11-15 12:44:12 +0100

    added javadoc

--- server/src/main/java/com/p2pmessenger/server/Server.java
+++ server/src/main/java/com/p2pmessenger/server/Server.java
@@ -1,6 +1,11 @@
 package com.p2pmessenger.server;
 
 public class Server {
+
+    /*
+     * 
+     * @param args
+    */
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
