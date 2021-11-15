0d20651d0bdaf1ed30f6793482a21259ff75da9b
aarondominguezcostas <aarondominguezcostas@gmail.com>
2021-11-15 17:33:43 +0100

    Creado Cliente

--- client/src/main/java/com/p2pmessenger/client/Client.java
+++ client/src/main/java/com/p2pmessenger/client/Client.java
@@ -1,7 +1,7 @@
-package com.p2pmessenger.server;
+package com.p2pmessenger.client;
 
-public class Server {
-
+public class Client {
+    
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
