44bee2511a6722b8778b38c804323d66a242e7a3
aarondominguezcostas <aarondominguezcostas@gmail.com>
2021-11-16 01:56:58 +0100

    Added Server-database connectivity

---
+++ server/src/main/java/com/p2pmessenger/server/P2PServerImpl.java
@@ -0,0 +1,29 @@
+package com.p2pmessenger.server;
+
+import com.mongodb.client.*;
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import java.rmi.server.*;
+import java.rmi.*;
+
+public class P2PServerImpl extends UnicastRemoteObject implements P2PServerInterface {
+
+    // constructor
+    public P2PServerImpl() throws RemoteException {
+        try {
+
+            //Conexion a la base de datos
+            ConnectionString connectionString = new ConnectionString("mongodb+srv://dbadmin:rZ9vnPsmch3T7z7R@p2puserdatabase.lzdps.mongodb.net/p2pdb?retryWrites=true&w=majority");
+            MongoClientSettings settings = MongoClientSettings.builder().applyConnectionString(connectionString).build();
+            MongoClient mongoClient = MongoClients.create(settings);
+            MongoDatabase database = mongoClient.getDatabase("p2pdb");
+
+            System.out.println("Conectado a base de datos: " + database.getName());
+
+            mongoClient.close();
+
+        } catch (Exception e) {
+            System.out.println("Error al conectar con la base de datos " + e.toString());
+        }
+    }
+}
--- server/src/main/java/com/p2pmessenger/server/Server.java
+++ server/src/main/java/com/p2pmessenger/server/Server.java
@@ -1,8 +1,39 @@
 package com.p2pmessenger.server;
 
+//imports
+import java.rmi.*;
+import java.rmi.registry.Registry;
+import java.rmi.registry.LocateRegistry;
+import java.io.*;
+
 public class Server {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        InputStreamReader is = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(is);
+        String portNum, registryURL;
+        try {
+            System.out.println("Introduce el puerto del registro RMI");
+            portNum = (br.readLine()).trim();
+            int RMIPortNum = Integer.parseInt(portNum);
+            startRegistry(RMIPortNum);
+            P2PServerImpl p2pServer = new P2PServerImpl();
+            registryURL = "rmi://localhost:" + portNum + "/mainServer";
+            Naming.rebind(registryURL, p2pServer);
+            System.out.println("Servidor P2P listo");
+        } catch (Exception e) {
+            System.out.println("Exception en ejecucion del servidor: " + e.toString());
+        }
+    }
+
+    // funcion que inicializa el registro RMI
+    private static void startRegistry(int RMIPortNum) throws RemoteException {
+        try {
+            Registry registry = LocateRegistry.getRegistry(RMIPortNum);
+            registry.list();
+
+        } catch (RemoteException e) {
+            LocateRegistry.createRegistry(RMIPortNum);
+        }
     }
 }
