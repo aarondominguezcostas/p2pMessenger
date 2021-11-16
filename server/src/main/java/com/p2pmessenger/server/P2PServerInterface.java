e4f1b14e833cdcb4ec1420560966de9c8c1382df
eloicl <corraleloi@gmail.com>
2021-11-16 12:57:04 +0100

    server Interface0.0

--- server/src/main/java/com/p2pmessenger/server/P2PServerInterface.java
+++ server/src/main/java/com/p2pmessenger/server/P2PServerInterface.java
@@ -13,5 +13,10 @@ public interface P2PServerInterface extends Remote{
     verifico que o solicitante é quen dice ser. Non sei se será necesario enviar a interface
     cliente, xa que poderíase buscar solo por id*/
     public void solicitarAmistad(String idDestinatario,Client_Interface cliente, String id, String contraseña)throws java.rmi.RemoteException;
+    //Método para actualizar unha solicitud de amistad no server
+    /*2 alternativas ao aceptar, ou actualizamos directamente dende o cliente a súa lista de 
+    amigos e logo actualizala no server, ou actualizala no server e que o server se encargue de
+    actualizala no cliente*/
     public void aceptarSolicitud(String idAceptante, String idSolicitante) throws java.rmi.RemoteException;
+    
 }
