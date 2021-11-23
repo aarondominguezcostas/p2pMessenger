cefc3d5877c10dab449cc41b4186a7d85c20fcef
eloicl <corraleloi@gmail.com>
2021-11-23 19:05:09 +0100

    Solicitudes(falla actualizacion)+enviarPeticion

---
+++ client/src/main/java/com/p2pmessenger/gui/tablaSolicitudes.java
@@ -0,0 +1,64 @@
+package com.p2pmessenger.gui;
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author ecl
+ */
+import java.util.ArrayList;
+import java.util.Set;
+import javax.swing.table.AbstractTableModel;
+
+/**
+ *
+ * @author ecl
+ */
+public class tablaSolicitudes extends AbstractTableModel {
+
+    private ArrayList<String> solicitudes;
+
+    public void ModeloTablaUsuarios(ArrayList<String> sol) {
+        this.solicitudes=sol;
+    }
+
+    public int getColumnCount() {
+        return 1;
+    }
+
+    @Override
+    public int getRowCount() {
+        return solicitudes == null ? 0 : solicitudes.size();
+    }
+
+    @Override
+    public String getColumnName(int columnNumber) {
+        
+        return "Solicitudes";
+    }
+
+    @Override
+    public Class getColumnClass(int columnNumber) {
+        Class clase = null;
+
+        return java.lang.String.class;
+    }
+
+    @Override
+    public boolean isCellEditable(int row, int col) {
+        return false;
+    }
+
+    public Object getValueAt(int row, int col) {
+        
+        return solicitudes.get(row);
+    }
+
+    public void setFilas(ArrayList<String> sol) {
+        this.solicitudes=sol;
+        fireTableDataChanged();
+    }
+}
+
